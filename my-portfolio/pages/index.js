96cab2f9e0c560b2c8840d6bf520a5ef9c975c82
Joshua Vayer <joshua.vayer@outlook.com>
2021-03-25 16:43:56 +0100

    PT.2_Layouts working

--- my-portfolio/pages/index.js
+++ my-portfolio/pages/index.js
@@ -1,8 +1,28 @@
-// /pages/index.js
+// pages/index.js
+import Layout from '../components/Layout';
 export default function Home() {
+  const entries = ['Project 1', 'Project 2', 'Project 3', 'Project 4'];
   return (
-    <div className="container">
-      Is this working?
-    </div>
+    <Layout>
+      <div className="entries">
+        <div className="row justify-content-start  ">
+          {entries.map((entry) => (
+            <div className="col-md-6">
+              <div className="entry mb-3">
+                <div className="main-image">
+                  <img src="https://via.placeholder.com/600x400" />
+                  <h1>{entry}</h1>
+                  <p>
+                    About
+                    {' '}
+                    {entry}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Layout>
   );
-};
+}
