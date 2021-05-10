9c3dc85a86b8ec9a8c99ebefca338b40a8d5510a
Joshua Vayer <joshua.vayer@outlook.com>
2021-05-10 21:19:36 +0200

    git branch

--- portfolio-backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b0b6a462d655f8936f6ebf17018b306b'),
-    },
-  },
-});
--- portfolio-backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'a591bddc-5b2f-4a8a-8873-4dd4e81f9ed5'
-};
